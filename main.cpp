af4e18d6d6e103b586e93eadff5b55efc90161eb
QuintinZong <Quintin0510@outlook.com>
2025-04-13 18:33:11 +0800

    Update main.cpp
    改变结束程序，可单独运行exe程序

--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
 #include<bits/stdc++.h>
-#include<windows.h>
 using namespace std;
 int main(){
 	double a,b,c,delta=0,x1=0,x2=0;
@@ -8,20 +7,20 @@ int main(){
 	cout<<"Thinking......\n";
 	delta=b*b-4*a*c;
 	if(delta<0){
-		cout<<"原方程无解; 
-		return 0;
+		cout<<"原方程无解"<<endl; 
+		system("pause");
 	}
 	else if(delta==0){
 		x1=x2=-b/2*a;
 		printf("x1=x2=%.2f",x1);
-		return 0;
+		cout<<endl;
+		system("pause");
 	}
 	else if(delta>0){
 		x1=(-b+sqrt(delta))/2*a;
 		x2=(-b-sqrt(delta))/2*a;
 		printf("x1=%.2f x2=%.2f",x1,x2);
-		return 0;
+		cout<<endl;
+		system("pause");
 	}
-	sleep 1000;
-	return 0;
 }
