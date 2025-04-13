aaa2852311e8c9c0aa76bb03b85bde163b0d2b7a
QuintinZong <Quintin0510@outlook.com>
2025-04-13 18:14:26 +0800

    Add files via upload

---
+++ 一元二次方程.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	double a,b,c,delta=0,x1=0,x2=0;
+	cout<<"请输入一元二次方程的各项系数:\n"; 
+	cin>>a>>b>>c;
+	cout<<"Thinking......\n";
+	delta=b*b-4*a*c;
+	if(delta<0){
+		cout<<"原方程无解"; 
+		return 0;
+	}
+	else if(delta==0){
+		x1=x2=-b/2*a;
+		printf("x1=x2=%.2f",x1);
+		return 0;
+	}
+	else if(delta>0){
+		x1=(-b+sqrt(delta))/2*a;
+		x2=(-b-sqrt(delta))/2*a;
+		printf("x1=%.2f x2=%.2f",x1,x2);
+		return 0;
+	}
+	return 0;
+}
